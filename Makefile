379b49e33c3452308efebc32438a2dd11b9875b5
moumniucf <youssef18moumni@gmail.com>
2025-01-15 15:28:58 +0100

    gg

--- Makefile
+++ Makefile
@@ -1,5 +1,26 @@
 NAME = push_swap
+
 CC = cc
+
 CFLAGS = -Wall -Wextra -Werror
-SRC = 
+
 HEADER = push_swap.h
+
+SRC = ft_isdigit.c
+
+OBG = $(SRC:%.c=%.o)
+
+all : $(NAME)
+
+%.o : %.c $(HEADER)
+	@$(CC) -c $< $(CFLAGS) -o $@
+	@echo "compiling" $< "to" $@
+
+$(NAME) : $(OBG)
+	@ar rc $(NAME) $(OBG)
+clean : 
+	@rm -f $(OBG)
+fclean :
+	@rm -f $(NAME)
+re : fclean all
+PHONY : clean fclean
---
+++ push_swap.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: youmoumn <youmoumn@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/15 11:22:53 by youmoumn          #+#    #+#             */
+/*   Updated: 2025/01/15 15:26:46 by youmoumn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+#define PUSH_SWAP_H
+
+typedef
+#include <libc.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+int	ft_isdigit(int c);
+#endif
