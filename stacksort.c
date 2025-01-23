9f9b2982890643ff6c05c608fdf2cd3a337bb4ff
moumniucf <youssef18moumni@gmail.com>
2025-01-23 19:24:15 +0100

    gg

--- stacksort.c
+++ stacksort.c
@@ -1,24 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.c                                        :+:      :+:    :+:   */
+/*   stacksort.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: youmoumn <youmoumn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/15 11:53:30 by youmoumn          #+#    #+#             */
-/*   Updated: 2025/01/23 17:42:05 by youmoumn         ###   ########.fr       */
+/*   Created: 2025/01/23 18:17:47 by youmoumn          #+#    #+#             */
+/*   Updated: 2025/01/23 18:17:59 by youmoumn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-int main(int ac, char **av)
-{
-	(void)ac;
-	int i = 1;
-	char **ptr = ft_split(av[i], ' ');
-	while(*ptr)
-	{
-		printf("%s\n", ptr[i++]);
-		ptr++;
-	}
-}
