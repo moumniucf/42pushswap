bb735013e8dbe1d0f49d3661dec9322beb44cd26
moumniucf <youssef18moumni@gmail.com>
2025-01-26 17:11:34 +0100

    42

--- push_ab.c
+++ push_ab.c
@@ -6,7 +6,7 @@
 /*   By: youmoumn <youmoumn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/18 10:21:15 by youmoumn          #+#    #+#             */
-/*   Updated: 2025/01/26 13:20:02 by youmoumn         ###   ########.fr       */
+/*   Updated: 2025/01/26 16:55:03 by youmoumn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,44 +44,44 @@ void	pb(stack **a, stack **b)
 	write(1, "pb\n", 3);
 }
 
-int main()
-{
-	stack *node1 = new_node(5);
-	stack *node2 = new_node(6);
-	stack *node3 = new_node(7);
-	stack *node4 = new_node(8);
-	stack *stacka = node1;
-	node1->next = node2;
-	node1->prev = NULL;
-	node2->next = node3;
-	node2->prev = node1;
-	node3->next = node4;
-	node3->prev = node2;
-	node4->next = NULL;
-	node4->prev = node3;
-	stack *nodea = new_node(1);
-	stack *nodeb = new_node(2);
-	stack *nodec = new_node(3);
-	stack *noded = new_node(4);
-	stack *stackb = nodea;
-	nodea->next = nodeb;
-	nodea->prev = NULL;
-	nodeb->next = nodec;
-	nodeb->prev = nodea;
-	nodec->next = noded;
-	nodec->prev = nodeb;
-	noded->next = NULL;
-	noded->prev = nodec;
-	pb(&stacka, &stackb);
-	while(stacka)
-	{
-		printf("|%d|\n", stacka->data);
-		stacka = stacka->next;
-	}
-	printf("========\n");
-	while(stackb)
-	{
-		printf("|%d|\n", stackb->data);
-		stackb = stackb->next;
-	}
-}
+// int main()
+// {
+// 	stack *node1 = new_node(5);
+// 	stack *node2 = new_node(6);
+// 	stack *node3 = new_node(7);
+// 	stack *node4 = new_node(8);
+// 	stack *stacka = node1;
+// 	node1->next = node2;
+// 	node1->prev = NULL;
+// 	node2->next = node3;
+// 	node2->prev = node1;
+// 	node3->next = node4;
+// 	node3->prev = node2;
+// 	node4->next = NULL;
+// 	node4->prev = node3;
+// 	stack *nodea = new_node(1);
+// 	stack *nodeb = new_node(2);
+// 	stack *nodec = new_node(3);
+// 	stack *noded = new_node(4);
+// 	stack *stackb = nodea;
+// 	nodea->next = nodeb;
+// 	nodea->prev = NULL;
+// 	nodeb->next = nodec;
+// 	nodeb->prev = nodea;
+// 	nodec->next = noded;
+// 	nodec->prev = nodeb;
+// 	noded->next = NULL;
+// 	noded->prev = nodec;
+// 	pb(&stacka, &stackb);
+// 	while(stacka)
+// 	{
+// 		printf("|%d|\n", stacka->data);
+// 		stacka = stacka->next;
+// 	}
+// 	printf("========\n");
+// 	while(stackb)
+// 	{
+// 		printf("|%d|\n", stackb->data);
+// 		stackb = stackb->next;
+// 	}
+// }
--- sortingtfv.c
+++ sortingtfv.c
@@ -6,7 +6,7 @@
 /*   By: youmoumn <youmoumn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 17:29:54 by youmoumn          #+#    #+#             */
-/*   Updated: 2025/01/26 13:21:36 by youmoumn         ###   ########.fr       */
+/*   Updated: 2025/01/26 17:08:32 by youmoumn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,51 +14,60 @@
 #include "push_swap.h"
 void	sortthree(stack **head)
 {
-	stack *first = (*head);
-	stack *second = first->next;
-	stack *last = second->next;
-	if((first->data) > (second->data) && (second->data) > (last->data) && (first->data) < (last->data))
-	{
+	int first = (*head)->data;
+	int second = (*head)->next->data;
+	int last = (*head)->next->next->data;
+	if (first > second && second > last && first < last)
 		sa(head); //2.1.3
-	}
-	if ((first->data) > (second->data) && (second->data) > (last->data))
+	if (first > second && second > last)
 	{
 		sa(head);
 		rra(head);//3.2.1
 	}
-	if ((first->data) > (second->data) && (second->data) < (last->data))
-	{
+	if (first > second && second < last)
 		ra(head);//3.1.2
-	}
-	if ((first->data) < (second->data) && (second->data) > (last->data) && (first->data) < (last->data))
+	if (first < second && second > last && first < last)
 	{
 		sa(head);
 		ra(head);//1.3.2
 	}
-	if ((first->data) < (second->data) && (second->data) > (last->data))
-	{
+	if (first < second && second > last)
 		rra(head);//2.3.1
-	}
 }
+
 void	sortfour(stack **head)
 {
-	stack *first;
-	stack *second;
-	stack *last;
+	stack *first = (*head);
+	stack *second = first->next;
+	stack *last = second->next;
+	stack *four = last->next;
+	if ((first->data) < (second->data) && (first->data) < (last->data) && (first->data) < (four->data))
+	{
+		sortthree(head);
+	}
+	if ((first->data) > (second->data) && (last->data) > (second->data) && (four->data) > (second->data))
+	{
+		
+	}
 }
+
 int main()
 {
-	stack *node1 = new_node(3);
-	stack *node2 = new_node(2);
-	stack *node3 = new_node(3);
+	stack *node1 = new_node(0);
+	stack *node2 = new_node(1);
+	stack *node3 = new_node(-3);
+	// stack *node4 = new_node(2);
 	stack *head = node1;
 	node1->next = node2;
 	node1->prev = NULL;
 	node2->next = node3;
 	node2->prev = node1;
-	node3->next = NULL;
+	// node3->next = node4;
 	node3->prev = node2;
+	// node4->prev = node3;
+	// node4->next = NULL;
 	sortthree(&head);
+	// sortfour(&head);
 	while(head)
 	{
 		printf("%d\n",head->data);
