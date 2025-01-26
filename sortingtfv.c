19849ef226f6bb64a4a2307c76d1a242a8a03bc1
moumniucf <youssef18moumni@gmail.com>
2025-01-26 13:21:45 +0100

    gf

--- sortingtfv.c
+++ sortingtfv.c
@@ -6,7 +6,7 @@
 /*   By: youmoumn <youmoumn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 17:29:54 by youmoumn          #+#    #+#             */
-/*   Updated: 2025/01/25 20:16:31 by youmoumn         ###   ########.fr       */
+/*   Updated: 2025/01/26 13:21:36 by youmoumn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,38 @@ void	sortthree(stack **head)
 	stack *last = second->next;
 	if((first->data) > (second->data) && (second->data) > (last->data) && (first->data) < (last->data))
 	{
-		sa(head);
+		sa(head); //2.1.3
 	}
-	else if ((first->data) > (second->data) > (last->data))
+	if ((first->data) > (second->data) && (second->data) > (last->data))
 	{
-		// sa(head);
-		rra(head);
+		sa(head);
+		rra(head);//3.2.1
 	}
+	if ((first->data) > (second->data) && (second->data) < (last->data))
+	{
+		ra(head);//3.1.2
+	}
+	if ((first->data) < (second->data) && (second->data) > (last->data) && (first->data) < (last->data))
+	{
+		sa(head);
+		ra(head);//1.3.2
+	}
+	if ((first->data) < (second->data) && (second->data) > (last->data))
+	{
+		rra(head);//2.3.1
+	}
+}
+void	sortfour(stack **head)
+{
+	stack *first;
+	stack *second;
+	stack *last;
 }
 int main()
 {
-	stack *node1 = new_node(1);
-	stack *node2 = new_node(3);
-	stack *node3 = new_node(2);
+	stack *node1 = new_node(3);
+	stack *node2 = new_node(2);
+	stack *node3 = new_node(3);
 	stack *head = node1;
 	node1->next = node2;
 	node1->prev = NULL;
